f288f130f1d6605f22921d17f7a7983b4ee3e6ac
huangkewei123 <20224397@qq.com>
2018-07-05 16:37:12 +0800

    SpringBoot2.0+Redis+MyBatis+shiro+pageHelp2

--- src/main/java/com/example/demo/jee/cache/CacheConfig.java
+++ src/main/java/com/example/demo/jee/cache/CacheConfig.java
@@ -1,5 +1,6 @@
 package com.example.demo.cache;
 
+import com.example.demo.jee.cache.FastJsonRedisSerializer;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
