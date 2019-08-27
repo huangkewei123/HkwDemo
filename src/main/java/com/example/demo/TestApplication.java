dae12c04a1d43284183e596809ed744a734d3bf7
huangkewei123 <20224397@qq.com>
2019-08-27 17:05:35 +0800

    提交修改

--- src/main/java/com/example/demo/TestApplication.java
+++ src/main/java/com/example/demo/TestApplication.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 
-@RunWith(SpringRunner.class)
+@RunWith(value = SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestApplication {
 
