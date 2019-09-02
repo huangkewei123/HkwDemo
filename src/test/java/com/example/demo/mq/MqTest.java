efa36717c076adfa9bf22ef1f12ad821ba846706
huangkewei123 <huangkewei>
2019-09-02 20:11:15 +0800

    提交rabbitMq

---
+++ src/test/java/com/example/demo/mq/MqTest.java
@@ -0,0 +1,12 @@
+package com.example.demo.mq;
+
+import org.junit.Test;
+
+public class MqTest {
+//    @Test
+//    public void oneToMany() throws Exception {
+//        for (int i=0;i<100;i++){
+//            neoSender.send(i);
+//        }
+//    }
+}
